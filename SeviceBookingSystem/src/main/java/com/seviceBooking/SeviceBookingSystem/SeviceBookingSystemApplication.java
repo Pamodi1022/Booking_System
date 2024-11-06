a58a2d0e531e612c57c1c5fb4a19dcd50dced85b
Pamodi1022 <pamodipansiluni1022@gmail.com>
2024-11-07 01:21:41 +0530

    Initial commit: Create new Booking System project with Spring Boot backend and Angular frontend. Set up database connection.

---
+++ SeviceBookingSystem/src/main/java/com/seviceBooking/SeviceBookingSystem/SeviceBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.seviceBooking.SeviceBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeviceBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeviceBookingSystemApplication.class, args);
+	}
+
+}
